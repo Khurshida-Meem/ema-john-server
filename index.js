e00710eaf351765933923fdfff08927a42ecadbe
Khurshida-Meem <meemrumi59@gmail.com>
2021-10-29 17:53:26 +0600

    added express mongo

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0yaa9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('online_shop')
+        const productsCollection = database.collection('products');
+
+        // GET Products API
+        app.get('/products', async (req, res) => {
+            // for query of link,
+            // console.log(req.query);
+
+            const cursor = productsCollection.find({});
+            //  pagination purpose
+            const page = req.query.page;
+            const size = parseInt(req.query.size);
+            let products;
+            const count = await cursor.count();
+            if (page) {
+                products = await cursor.skip(page * size).limit(size).toArray();
+            }
+            else {
+                products = await cursor.toArray();
+            }
+
+            res.send({
+                count,
+                products
+            });
+        })
+
+        // use post to get data by keys
+        app.post('/products/byKeys', async (req, res) => {
+            const keys = req.body;
+            // database er moddhe key field er moddhe keys jegulor moddhe ase segula select korbe like for in
+            const query = { key: { $in: keys } }
+            const products = await productsCollection.find(query).toArray();
+            res.send(products);
+        });
+
+    }
+    finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir)
+app.get('/', (req, res) => {
+    res.send('Ema john is running');
+});
+
+app.listen(port, () => {
+    console.log('server running at port: ', port);
+})
